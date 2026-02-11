22494cc4f01ec8fb876481d2b6274719cf5fbe1e
Natan Gomes <gomesnatan84@gmail.com>
2026-02-11 20:09:34 -0300

    configuracao base

---
+++ src/test/java/br/com/delivery/pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivery.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
